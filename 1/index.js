b05ffe4fde9b9850119d52f801998b7279ea550c
leighwhite96 <31276731+leighwhite96@users.noreply.github.com>
2020-12-02 10:27:37 +0000

    map through a parsed array

--- 1/index.js
+++ 1/index.js
@@ -3,15 +3,16 @@ const fs = require('fs');
 const data = fs.readFileSync('./input.txt', 'utf8').split('\n');
 
 const findNumbers = arr => {
-    let product; 
+    const parsedArray = arr.map(num => parseInt(num));
+    let product;
 
-    for (let i = 0; i < arr.length; i += 1) {
-        for (let j = i + 1; j < arr.length; j += 1) {
-            for (let k = 0; k < arr.length; k += 1) {
+    for (let i = 0; i < parsedArray.length; i += 1) {
+        for (let j = i + 1; j < parsedArray.length; j += 1) {
+            for (let k = 0; k < parsedArray.length; k += 1) {
                 if (k === i || k === j) break;
 
-                if (parseInt(arr[i]) + parseInt(arr[j]) + parseInt(arr[k]) === 2020) {
-                    product = parseInt(arr[i]) * parseInt(arr[j]) * parseInt(arr[k]);
+                if (parsedArray[i] + parsedArray[j] + parsedArray[k] === 2020) {
+                    product = parsedArray[i] * parsedArray[j] * parsedArray[k];
                     
                     break;
                 }
